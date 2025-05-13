b8a8ea337805424a2edd19b20ac859a11b3748fc
Momchil Tsanov <momchil395@gmail.com>
2025-05-13 21:51:21 +0300

    user session manager tests done

---
+++ src/test/java/org/core/UserSessionManagerTest.java
@@ -0,0 +1,42 @@
+package org.core;
+
+import org.entities.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserSessionManagerTest {
+
+    private UserSessionManager sessionManager;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        sessionManager = new UserSessionManager();
+        user = new User("testuser1", "123456");
+    }
+
+    @Test
+    void initiallyNoActiveSession() {
+        assertFalse(sessionManager.hasActiveSession());
+        assertNull(sessionManager.getActiveSession());
+    }
+
+    @Test
+    void setAndGetActiveSession() {
+        sessionManager.setActiveSession(user);
+        assertTrue(sessionManager.hasActiveSession());
+        assertEquals(user, sessionManager.getActiveSession());
+    }
+
+    @Test
+    void terminateActiveSession() {
+        sessionManager.setActiveSession(user);
+        assertTrue(sessionManager.hasActiveSession());
+        sessionManager.terminateActiveSession();
+        assertFalse(sessionManager.hasActiveSession());
+        assertNull(sessionManager.getActiveSession());
+    }
+}
+
